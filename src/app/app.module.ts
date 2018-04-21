7f735ebf2e0fd91f3458299bdd4dc0db9537e501
Muhammad Hazim <mhazim2@gmail.com>
2018-04-21 16:28:06 +0700

    repair quotemark

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { EasyUIModule } from "ng-easyui/components/easyui/easyui.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
+  bootstrap: [
+    AppComponent
+  ],
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    FormsModule,
+    BrowserModule,
+    EasyUIModule
+  ]
 })
 export class AppModule { }
